3b7344953ff04ca2f92b1b093cb926e82de217c9
calr0x <calr0x@gmail.com>
2021-06-19 11:43:06 -0500

    .

---
+++ bid_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script checks the logs for "Accepting" which indicates a bid.
+# Setting to change:
+# BID_INTERVAL: Set this to how far back in minutes to search the log for mentions of "Accepting".
+# This value should match the CRON schedule. For example, Every 1 hour
+# CRON should run this script which checks the logs for the past 1 hour.
+
+BID_INTERVAL="1h"
+
+BIDS=$(journalctl -u otnode.service --since "15 minutes ago" | grep Accepting | wc -l)
+echo $BIDS
+
+if [ $BIDS -eq 0 ]; then
+  /root/OT-NodeWatch/data/send.sh "Has not bid in the last $BID_INTERVAL"
+fi
