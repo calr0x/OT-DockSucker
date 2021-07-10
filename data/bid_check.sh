5809b30fec25e65f0d90d109ea0ebfcdb128f68c
Valcyclovir <85747941+Valcyclovir@users.noreply.github.com>
2021-07-10 00:58:37 -0400

    Update bid_check.sh

--- data/bid_check.sh
+++ data/bid_check.sh
@@ -2,25 +2,34 @@
 
 # This script checks the logs for "Accepting" which indicates a bid.
 # Setting to change:
-# BID_INTERVAL: Set this to how far back in minutes to search the log for mentions of "Accepting".
+# BID_CHECK_ENABLED: Set to false to disable bid notifications.
+# CHECK_INTERVAL: Set this to how far back to search the log for mentions of "Accepting".
 # This value should match the CRON schedule. For example, Every 1 hour
 # CRON should run this script which checks the logs for the past 1 hour.
 
-BID_INTERVAL="1h"
+CHECK_INTERVAL="1 hour ago"
+BID_CHECK_ENABLED="true"
 
-BIDS=$(journalctl -u otnode.service --since "1 hour ago" | grep Accepting | wc -l)
-echo $BIDS
+BIDS=$(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep Accepting | wc -l)
+#echo Bids: $BIDS
 
 if [ $BIDS -eq 0 ]; then
-  /root/OT-NodeWatch/data/send.sh "Has not bid in the last $BID_INTERVAL"
+  /root/OT-NodeWatch/data/send.sh "Has not bid since $CHECK_INTERVAL"
 fi
 
-BIDS=$(journalctl -u otnode.service --since "1 hour ago" | grep 've been chosen' | wc -l)
-echo $BIDS
+JOBS=$(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep 've been chosen' | wc -l)
+#echo Jobs: $JOBS
 
-if [ $BIDS == 1 ]; then
-  /root/OT-NodeWatch/data/send.sh "Job awarded"
-fi
-if [ $BIDS -ge 2 ]; then
-  /root/OT-NodeWatch/data/send.sh "$BIDS job awarded"
+OFFER_ID=($(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep 've been chosen' | grep -Eo '0x[a-z0-9]+'))
+
+#echo Array: ${#OFFER_ID[@]}
+if [ $BID_CHECK_ENABLED == "true" ]
+then
+  for i in "${OFFER_ID[@]}"
+  do
+    TOKEN_ARRAY=($(curl -s -X GET "https://v5api.othub.info/api/Job/detail/$i" -H  "accept: text/plain" | cut -d',' -f 54 | cut -d'"' -f 4))
+    JOBTIME_ARRAY=($(curl -s -X GET "https://v5api.othub.info/api/Job/detail/$i" -H  "accept: text/plain" | cut -d',' -f 53 | cut -d':' -f 2))
+    DAYS=$(expr ${JOBTIME_ARRAY[@]} / 60 / 24)
+    /root/OT-NodeWatch/data/send.sh "Job awarded: $DAYS days at ${TOKEN_ARRAY[@]} TRAC"
+  done
 fi
