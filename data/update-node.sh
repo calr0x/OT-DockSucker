94377dcd95ccf051a833a0d4362de1b0747f3cc1
calr0x <calr0x@gmail.com>
2021-08-06 10:58:52 -0500

    .

--- data/update-node.sh
+++ data/update-node.sh
@@ -2,23 +2,23 @@
 #
 systemctl stop otnode
 
-NEW_VERSION=$(curl -sL https://api.github.com/repos/origintrail/ot-node/releases/latest | jq -r .tag_name)
-OLD_VERSION=Whaever the original dir was named
+#NEW_VERSION=$(curl -sL https://api.github.com/repos/origintrail/ot-node/releases/latest | jq -r .tag_name)
+#OLD_VERSION=Whaever the original dir was named
 
 cd /ot-node/
 
 git clone https://github.com/OriginTrail/ot-node.git
 
-mv /ot-node/ot-node /ot-node/$VERSION
+mv /ot-node/ot-node /ot-node/5.1.0
 
-rm /root/current
+rm /ot-node/current
 
-ln -s /ot-node/$VERSION /ot-node/current
+ln -s /ot-node/5.1.0 /ot-node/current
 
 cd current
 
-cp /ot-node/$OLD_VERSION/.origintrail_noderc /ot-node/current/
-cp ../5.0.4/.env .
+cp /ot-node/5.0.4/.origintrail_noderc /ot-node/current/
+cp /ot-node/5.0.4/.env /ot-node/current
 
 npm install
 
