eaa145e0d9c24f395bb22b0bc663639b0f0c48a4
calr0x <calr0x@gmail.com>
2021-08-06 10:48:00 -0500

    .

--- data/update-node.sh
+++ data/update-node.sh
@@ -2,7 +2,7 @@
 #
 systemctl stop otnode
 
-NEW_VERSION=$(curl -sL https://api.github.com/repos/origintrail/ot-node/releases/latest | jq .tag_name)
+NEW_VERSION=$(curl -sL https://api.github.com/repos/origintrail/ot-node/releases/latest | jq -r .tag_name)
 OLD_VERSION=Whaever the original dir was named
 
 cd /ot-node/
@@ -15,6 +15,11 @@ rm /root/current
 
 ln -s /ot-node/$VERSION /ot-node/current
 
+cd current
+
 cp /ot-node/$OLD_VERSION/.origintrail_noderc /ot-node/current/
+cp ../5.0.4/.env .
+
+npm install
 
 systemctl start otnode
