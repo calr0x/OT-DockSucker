2b711be5a69de8d0106db51f803a9b66f30bafa6
calr0x <52664000+calr0x@users.noreply.github.com>
2021-09-03 06:34:20 -0500

    update update-node.sh

--- data/update-node.sh
+++ data/update-node.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-if [[ -d "/ot-node/5.1.0" ]]; then
+NEW_VERSION=$(curl -sL https://api.github.com/repos/origintrail/ot-node/releases/latest | jq -r .tag_name | sed 's|v||')
+OLD_VERSION=$(ls -l /ot-node/current | awk '{print $11}' | sed 's|/ot-node/||')
+
+if [[ -d "/ot-node/$NEW_VERSION" ]]; then
   echo "This node is already updated!"
-  exit 1
+  exit 2
 fi
-
-systemctl stop otnode
-
-#NEW_VERSION=$(curl -sL https://api.github.com/repos/origintrail/ot-node/releases/latest | jq -r .tag_name)
-#OLD_VERSION=Whaever the original dir was named
 
 cd /ot-node/
 
 git clone -b release/mainnet https://github.com/OriginTrail/ot-node.git
 
-mv /ot-node/ot-node /ot-node/5.1.0
+mv /ot-node/ot-node /ot-node/$NEW_VERSION
 
 rm /ot-node/current
 
-ln -s /ot-node/5.1.0 /ot-node/current
+ln -s /ot-node/$NEW_VERSION /ot-node/current
 
 cd current
 
-cp /ot-node/5.0.4/.origintrail_noderc /ot-node/current/
-cp /ot-node/5.0.4/.env /ot-node/current
+cp /ot-node/$OLD_VERSION/.origintrail_noderc /ot-node/current/
+cp /ot-node/$OLD_VERSION/.env /ot-node/current
 
 npm install
 
-systemctl start otnode
+systemctl restart otnode
 
 journalctl -u otnode -f | ccze -A
